f0195c1498785521c4f4b73e07fc32ca31ef3088
dotarsojat69 <tdeath457@gmail.com>
2024-05-22 17:20:21 +0700

    feat: finish task with dayjs

--- convertUnixToDate.js
+++ convertUnixToDate.js
@@ -1,11 +1,13 @@
-/*
-Gunakan npm dayjs (https://www.npmjs.com/package/dayjs) untuk menyelesaikan tugas ini.
-Convert Unix timestamp menjadi tanggal dengan format DD/MM/YYYY
-*/
+
+// Gunakan npm dayjs (https://www.npmjs.com/package/dayjs) untuk menyelesaikan tugas ini.
+// Convert Unix timestamp menjadi tanggal dengan format DD/MM/YYYY
+
+const dayjs = require('dayjs');
 
 function convertUnixToDate(unixTimestamp) {
-  // Code here
+  const date = dayjs.unix(unixTimestamp).format('DD/MM/YYYY');
+  return date;
 }
 
-convertUnixToDate(1710867600) // Output: 20/03/2024
-convertUnixToDate(1714150800) // Output: 27/04/2024
+console.log(convertUnixToDate(1710867600)) // Output: 20/03/2024
+console.log(convertUnixToDate(1714150800)) // Output: 27/04/2024
