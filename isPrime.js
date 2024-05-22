4e661a8b504b99110b4777870bdea9beb67969a5
dotarsojat69 <tdeath457@gmail.com>
2024-05-22 17:20:56 +0700

    feat: finish task find prime number

--- isPrime.js
+++ isPrime.js
@@ -4,7 +4,21 @@ Jika bilangan prima maka return true, jika bukan return false.
 */
 
 function isPrime(num) {
-  // Code here
+  if (num <= 1) {
+    return false;
+  }
+  if (num <= 3) {
+    return true;
+  }
+  if (num % 2 === 0 || num % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(isPrime(7)); // Output: true
